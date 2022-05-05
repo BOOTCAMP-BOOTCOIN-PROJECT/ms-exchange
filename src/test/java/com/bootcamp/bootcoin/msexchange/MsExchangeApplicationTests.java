525fd4b39a2482d42ca1cc0f26ca1ee1abfd42e1
samuel14luis <luismendoza.samuel@gmail.com>
2022-05-05 11:25:39 -0500

    scaffolding

---
+++ src/test/java/com/bootcamp/bootcoin/msexchange/MsExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bootcoin.msexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
